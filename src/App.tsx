b0b33eadf351d0a68b1ba91609fcb8e3adb02e2f
xin <xinxibin@tydic.com>
2021-06-10 14:05:51 +0800

    1.添加 react-router-dom npm install react-router-dom 2.添加types npm install @types/react-router-dom --save-dev 3.封装首页，以路由的方式加载首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,14 @@
-import React from 'react'
 import styles from './App.module.css'
-import {
-  Header,
-  Footer,
-  Carousel,
-  SideMenu,
-  ProductCollection,
-  BusinessPartner,
-} from './components'
-import { Row, Col, Typography } from 'antd'
-import { productList1, productList2, productList3 } from './mockups'
-import sideImage from './assets/images/sider_2019_12-09.png'
-import sideImage2 from './assets/images/sider_2019_02-04.png'
-import sideImage3 from './assets/images/sider_2019_02-04-2.png'
 
-import sideImage11 from './assets/images/facebook-807588_640.png'
-import sideImage22 from './assets/images/follow-826033_640.png'
-import sideImage33 from './assets/images/icon-720944_640.png'
-import sideImage44 from './assets/images/microsoft-80658_640.png'
+import { HomePage } from './pages/home'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      {/* 页面内容 content */}
-      <div className={styles['page-content']}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              爆款推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              国内游推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
-        <BusinessPartner />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Route path="/" component={HomePage} />
+      </BrowserRouter>
     </div>
   )
 }
